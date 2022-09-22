96e7713179c56de0173cf4b0f4b15488bdc3410f
Octoping925 <mooner925@gmail.com>
2022-09-22 15:29:20 +0900

    Initial commit

---
+++ src/main/java/octoping/YoutubePlaylistRecommender/YoutubePlaylistRecommenderApplication.java
@@ -0,0 +1,13 @@
+package octoping.YoutubePlaylistRecommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubePlaylistRecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubePlaylistRecommenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/octoping/YoutubePlaylistRecommender/YoutubePlaylistRecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package octoping.YoutubePlaylistRecommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubePlaylistRecommenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
