984ad35678e705390de7244f9e8cf84d0910a9cc
Octoping925 <mooner925@gmail.com>
2022-09-23 17:58:24 +0900

    PlaylistController, Service 약간 수정

--- src/main/java/octoping/YoutubePlaylistRecommender/controller/PlaylistController.java
+++ src/main/java/octoping/YoutubePlaylistRecommender/controller/PlaylistController.java
@@ -1,7 +1,9 @@
 package octoping.YoutubePlaylistRecommender.controller;
 
+import java.io.IOException;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,17 +14,21 @@ import lombok.RequiredArgsConstructor;
 import octoping.YoutubePlaylistRecommender.common.utility.ResponseObject;
 import octoping.YoutubePlaylistRecommender.service.PlaylistService;
 
-@RequiredArgsConstructor
 @RestController
+@RequiredArgsConstructor
 public class PlaylistController {
     private final PlaylistService playlistService;
     
     @PostMapping("/playlist/{videoId}/crawl")
     public ResponseEntity<Map<String, Object>> crawl(@PathVariable(name = "videoId") String videoId) {
-        playlistService.crawl(videoId);
-
-        return ResponseEntity.ok()
-            .body(ResponseObject.create(null, "크롤링 성공"));
+        try {
+            playlistService.crawl(videoId);
+            return ResponseEntity.ok()
+                .body(ResponseObject.create(videoId, "크롤링 성공"));
+        }
+        catch (IOException e) {
+            return new ResponseEntity<>(ResponseObject.create(null, e.getMessage()), HttpStatus.NOT_FOUND);
+        }
     }
 
     @GetMapping("/playlist/{videoId}/info")
--- src/main/java/octoping/YoutubePlaylistRecommender/service/PlaylistService.java
+++ src/main/java/octoping/YoutubePlaylistRecommender/service/PlaylistService.java
@@ -1,15 +1,21 @@
 package octoping.YoutubePlaylistRecommender.service;
 
+import java.io.IOException;
+import octoping.YoutubePlaylistRecommender.vo.PlaylistVO;
+import octoping.YoutubePlaylistRecommender.common.utility.Helper;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Service
+@RequiredArgsConstructor
 public class PlaylistService {
-    private final YoutubeAPIService youtubeAPIService;
+    private final YoutubeService youtubeService;
 
-    public void crawl(String videoId) {
+    public void crawl(String videoId) throws IOException {
+        PlaylistVO playlistVO = youtubeService.getYoutubeVideoOverview(videoId);
 
+        String koreanTitle = Helper.parseKorean(playlistVO.getTitle());
+        System.out.println(koreanTitle);
     }
 }
