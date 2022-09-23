4ac3dd5b8aff9f8f1c672bc7009dbba9e8413963
Octoping925 <mooner925@gmail.com>
2022-09-23 14:33:36 +0900

    FEAT: ApiConnection 클래스 추가

---
+++ src/main/java/octoping/YoutubePlaylistRecommender/common/utility/ApiConnection.java
@@ -0,0 +1,37 @@
+package octoping.YoutubePlaylistRecommender.common.utility;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.json.JSONObject;
+
+
+public class ApiConnection {
+
+    /**
+     * GET으로 API 통신을 보내는 함수
+     * @param apiUrl API 통신을 보낼 URI
+     * @return API 통신 결과 JSONObject
+     */
+    public static JSONObject getJson(String apiUrl) throws IOException {
+        URL url = new URL(apiUrl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-Type", "application/json");
+        conn.setDoOutput(true);
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+
+        while((line = br.readLine()) != null) {
+            sb.append(line);
+        }
+
+        return new JSONObject(sb.toString());
+    }
+
+}
